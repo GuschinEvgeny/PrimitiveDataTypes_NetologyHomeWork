33827557611d141a820b347897434c0faef1d937
Guschin Evgeny <eg@vidis.tech>
2023-09-14 17:59:47 +0300

    buil bonus calculation system

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Введите стоимость авиабилета: ");
+        Scanner price = new Scanner(System.in);
+        System.out.println("В этой строке: ");
+        int ticket_price = price.nextInt();
+
+        //int ticket_price = 17958;
+        byte bonus_mile = 20;
+        System.out.println("Количество ваших бонусных миль за текущий перелет");
+        System.out.println( "равно " + ticket_price /  bonus_mile );
     }
 }
