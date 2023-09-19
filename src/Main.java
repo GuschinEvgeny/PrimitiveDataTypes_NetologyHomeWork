3f5d4d71a181f22f78c2bca933bedf44063b3265
Guschin Evgeny <eg@vidis.tech>
2023-09-19 17:36:06 +0300

    delete ScannerUtilit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
-// import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Введите стоимость авиабилета: ");
-        //Scanner price = new Scanner(System.in);
-        //System.out.println("В этой строке: ");
-        //int ticket_price = price.nextInt();
         int ticketPrice = 13676 ;
         byte bonusMile = 20;
         System.out.println("Стоимость вашего перелета " + ticketPrice);
